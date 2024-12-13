77fb7f84095e1fa2af64dd915a28580ce2d55925
dhananjay431 <dbel431@gmail.com>
2024-12-13 21:24:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# new_pdf
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/dhananjay431/new_pdf)
---
+++ script.js
@@ -0,0 +1,53 @@
+var _base64 = '';
+$('#cng').click(function () {
+  let c = ['red', null, 'blue', null, 'green', 'yellow', null].sort(
+    () => 0.5 - Math.random()
+  );
+  showPdf(_base64, c[0]);
+});
+
+$('#file').change(async (ev) => {
+  _base64 = await getBase64(ev.target.files[0]);
+  showPdf(_base64, 'red');
+});
+function showPdf(pdfData, colorList = null) {
+  let container = document.getElementById('pdf-container');
+  container.innerHTML = '';
+  pdfjsLib.getDocument({ data: atob(pdfData) }).promise.then((pdf) => {
+    let pageNo = Math.floor(Math.random() * pdf._pdfInfo.numPages + 1);
+    pdf.getPage(pageNo).then(function (page) {
+      let canvas = document.createElement('canvas');
+      let context = canvas.getContext('2d');
+      container.appendChild(canvas);
+      const viewport = page.getViewport({ scale: 1.5 });
+      canvas.width = 700;
+      canvas.height = 900;
+      page
+        .render({ canvasContext: context, viewport: viewport })
+        .promise.then((resp) => {
+          if (colorList != null) {
+            context.strokeStyle = colorList;
+            context.strokeRect(rn(300), rn(300), rn(300), rn(300));
+            context.strokeRect(rn(300), rn(300), rn(300), rn(300));
+          }
+        });
+    });
+  });
+}
+function getBase64(pdfInput) {
+  return new Promise((a, b) => {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      a(e.target.result.split(',')[1]);
+    };
+    reader.onerror = function (error) {
+      console.error('Error reading the file:', error);
+      b(error);
+    };
+
+    reader.readAsDataURL(pdfInput);
+  });
+}
+function rn(num) {
+  return Math.floor(Math.random() * num + 1);
+}
